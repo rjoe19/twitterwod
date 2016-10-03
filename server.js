2c75eb96e4b67ae3d0823347214572310beb176a
Richard Joe <rjoe19@hotmail.com>
2016-10-03 17:27:26 +1300

    using w3 schools responsive design template

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+// require modules
 var express = require('express')
 var app = express()
 
+
+//Add in Middleware
+//==============================================================================
+app.use(express.static('client'));
+
+
+
+
+// Add in Routes
+//==============================================================================
 // Step 1 MVP = 'hello world'
 app.get('/', function (req, res) {
   res.send('Hello World')
