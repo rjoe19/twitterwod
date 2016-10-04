9956dc050ac703ebd0f2e159a9b1e873aac040cd
Richard Joe <rjoe19@hotmail.com>
2016-10-04 15:53:15 +1300

    update env details

--- server.js
+++ server.js
@@ -25,20 +25,21 @@ app.listen(3000)
 
 //load environmental variables:
 dotenv.load();
-console.log(process.env.TWITTER_API_KEY)
-console.log(process.env.TWITTER_API_SECRET)
-console.log(process.env.TWITTER_ACCESS_TOKEN)
-console.log(process.env.TWITTER_TOKEN_SECRET)
+// console.log(process.env.TWITTER_API_KEY)
+// console.log(process.env.TWITTER_API_SECRET)
+// console.log(process.env.TWITTER_ACCESS_TOKEN)
+// console.log(process.env.TWITTER_TOKEN_SECRET)
 
 
 
 // setup twitter API client call:
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
+var twitterClient = new Twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+	  access_token_secret: process.env.TWITTER_ACCESS_SECRET
+	})
+
 
     twitterClient.get('search/tweets', {q: '%23haiku', lang: 'en'}, function(err, tweet, response) {
   		  if (err) { console.log("Error retriving tweets"); return }
