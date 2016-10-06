b3ddd09d71013034fe57b0c2a58ae9b967e27701
Richard Joe <rjoe19@hotmail.com>
2016-10-06 21:41:25 +1300

    fixing error

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ var app = express // create an instance of express.
 
 //load environmental variables:
 dotenv.load();
-// console.log(process.env.TWITTER_API_KEY)
-// console.log(process.env.TWITTER_API_SECRET)
-// console.log(process.env.TWITTER_ACCESS_TOKEN)
-// console.log(process.env.TWITTER_TOKEN_SECRET)
+console.log(process.env.TWITTER_API_KEY)
+console.log(process.env.TWITTER_API_SECRET)
+console.log(process.env.TWITTER_ACCESS_TOKEN)
+console.log(process.env.TWITTER_TOKEN_SECRET)
 // successfully logs all keys/secrets/tokens from .env file for testing purposes
 
 
