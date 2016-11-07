99afdade54aca039911f7406460f0c60ae595f15
Richard Joe <rjoe19@hotmail.com>
2016-11-08 09:54:37 +1300

    all updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express(); // create an instance of express.
 
 //Add in Middleware
 //==============================================================================
-app.use(express.static('client'));
+// app.use(express.static('client'));
 
 
 
@@ -16,11 +16,11 @@ app.use(express.static('client'));
 // Add in Routes
 //==============================================================================
 // Step 1 MVP = 'hello world', done.
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(3000)
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+//
+// app.listen(3000)
 
 
 //load environmental variables:
@@ -33,21 +33,21 @@ dotenv.load();
 
 
 // setup twitter API client call:
-// var twitterClient = new Twitter({
-// 		consumer_key: process.env.TWITTER_CONSUMER_KEY,
-// 	  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-// 	  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-// 	  access_token_secret: process.env.TWITTER_ACCESS_SECRET
-// 	})
-//search query get request to twitter api.
-    // twitterClient.get('search/tweets', {q: '%23wod', lang: 'en'}, function(err, tweet, response) {
-  	// 	  if (err) {
-		// 			console.log("Error retrieving tweets my homie"); return
-		// 		}
-  	// 	    console.log(returnTweets(tweet));
-  	// 	});
+var twitterClient = new Twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+	  access_token_secret: process.env.TWITTER_ACCESS_SECRET
+	})
+// //search query get request to twitter api.
+//     twitterClient.get('search/tweets', {q: '%23wod', lang: 'en'}, function(err, tweet, response) {
+//   		  if (err) {
+// 					console.log("Error retrieving tweets my homie"); return
+// 				}
+//   		    console.log(returnTweets(tweet));
+//   		});
 
-		// var params = {screen_name: 'nodejs'};
+// 		var params = {screen_name: 'nodejs'};
 // twitterClient.get('search/tweets', {q:'%23wod', lang: 'en'}, function(error, tweets, response) {
 // 	if (error) {
 // 		console.log('ERROR RETRIEVING TWEETS', error);
